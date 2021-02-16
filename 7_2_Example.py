f6ee6b6f0c143c98fcc793bd95237df69e598372
Blaine-Mason <bmason3@gulls.salisbury.edu>
2021-02-16 10:38:59 -0500

    Time to make the library

---
+++ 7_2_Example.py
@@ -0,0 +1,9 @@
+# Import standard libraries
+import numpy as np
+import math
+
+# Import custom chapter 7 library
+import library.ch7lib as ch7
+
+print(ch7.hn_matrix(8))
+
--- starting_example_3-2.py
+++ starting_example_3-2.py
@@ -8,16 +8,16 @@ def f(x):
 def df(x):
     return math.exp(x) + 1
 
-#Fix this
+
+# Fix this
 def newtons_method(iterations, x_0):
-    x = [0]*iterations
-    for i in range(iterations):
-        if i == 0:
-            print(f'For initial guess: {x_0}')
+    x = [x_0] * iterations
+    for i in range(1, iterations):
+        if i == 1:
+            print(f'For initial guess: {x[i]}')
         else:
-            x[i] = x[i-1] - f(x[i-1]) / df(x[i-1])
-            print(f'For guess #{i}: {x[i + 1]}')
-            x[i + 1] = x[i] - f(x[i]) / df(x[i])
+            x[i] = x[i - 1] - f(x[i - 1]) / df(x[i - 1])
+            print(f'For guess #{i}: {x[i]}')
 
 
 if __name__ == '__main__':
